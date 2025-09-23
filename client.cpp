f46c376100be581703fd8b6022db9b0cfbfcf28b
Nagul75 <nagul697@gmail.com>
2025-09-23 11:11:34 +0530

    update to accomodate new read and write

--- client.cpp
+++ client.cpp
@@ -5,26 +5,69 @@
 #include "Socket.h"
 #include <iostream>
 
+bool handle_one_request(const Socket& client_socket, const std::string& message)
+{
+    try
+    {
+        // WRITE OPERATION
+        const uint32_t message_length{static_cast<uint32_t>(message.length())};
+        client_socket.write_all(&message_length, sizeof(message_length)); // write the length of the message first (header)
+        client_socket.write_all(message.c_str(), message.length()); // write the actual message now
+        std::cout << "Message sent \n";
+
+        // READ OPERATION
+        // read the first 4 header bytes
+        uint32_t len;
+        client_socket.read_full(&len, sizeof(len));
+        // read the message based on the length first read
+        std::vector<char> buffer(len);
+        client_socket.read_full(buffer.data(), len);
+        std::cout << "Server says: " << std::string(buffer.begin(), buffer.end()) << '\n';
+
+        return true;
+    }
+    catch (const std::runtime_error& e)
+    {
+        std::cerr << "Connection error: " << e.what() << '\n';
+        return false;
+    }
+}
+
 int main()
 {
     try
     {
         Socket client_socket{};
         std::cout << "Connecting to server ... \n";
+
         client_socket.connect_to("127.0.0.1", 1234);
+
         std::cout << "Connected successfully! \n";
 
-        const std::string message{"Heya, this is client! ^^ How are ya doing?!"};
-        client_socket.write_data(message);
-        std::cout << "Message sent \n";
-
-        std::vector<char> buffer(1024);
-        ssize_t bytes_read{client_socket.read_data(buffer)};
-
-        std::cout << "Server says: " << std::string(buffer.data(), bytes_read) << '\n';
-
-    }catch (const std::runtime_error& e)
+        if (!handle_one_request(client_socket, "First message"))
+        {
+            std::cout << "Exchange unsuccessful! \n";
+            return 1;
+        }
+        std::cout << "Exchange successful! \n";
+        if (!handle_one_request(client_socket, "Second message"))
+        {
+            std::cout << "Exchange unsuccessful! \n";
+            return 1;
+        }
+        std::cout << "Exchange successful! \n";
+        if (!handle_one_request(client_socket, "Second message"))
+        {
+            std::cout << "Exchange unsuccessful! \n";
+            return 1;
+        }
+        std::cout << "Exchange successful! \n";
+        return 0;
+    }
+    catch (const std::runtime_error& e)
     {
-        std::cerr << "Client error: "  << e.what() << '\n';
+        // This will now catch connection errors too
+        std::cerr << "Client error: " << e.what() << '\n';
+        return 1;
     }
 }
--- server.cpp
+++ server.cpp
@@ -4,26 +4,58 @@
 
 #include "Socket.h"
 
+bool handle_one_request(const Socket& client_socket)
+{
+    try
+    {
+        // read the first 4 header bytes
+        uint32_t len;
+        client_socket.read_full(&len, sizeof(len));
+
+        if (len > Socket::K_MAX_MSG)
+        {
+            std::cerr << "Message is too long! \n";
+            return false; // terminate connection
+        }
+        std::vector<char>body_buf(len);
+        client_socket.read_full(body_buf.data(), len);
+
+        std::cout << "Client says: " << std::string(body_buf.begin(), body_buf.end()) << '\n';
+
+        // response
+        const std::string response{"Moshi Moshi!!! This is the server ^^! How are you?!"};
+        const uint32_t reply_length{static_cast<uint32_t>(response.length())};
+
+        client_socket.write_all(&reply_length, sizeof(reply_length));
+        client_socket.write_all(response.c_str(), response.length());
+
+        return true;
+    }
+    catch (const std::runtime_error& e)
+    {
+        std::cerr << "Connection error: " << e.what() << '\n';
+        return false;
+    }
+
+}
+
 void handleClient(Socket client_socket)
 {
-    std::cout << "Handling new client connection. \n";
-    std::vector<char> buffer(1024);
-    ssize_t bytes_read{client_socket.read_data(buffer)};
-    if (bytes_read > 0)
+    std::cout << "Handling new client connection ... \n";
+    while (handle_one_request(client_socket))
     {
-        std::cout << "Client says: " << std::string(buffer.data(), bytes_read) << '\n';
-
-        const std::string response {"Response from server hello!!!!!! :)"};
-        client_socket.write_data(response);
+        // continue serving requests
     }
-    std::cout << "Closing client connection. \n";
+
+    std::cout << "Closing client! \n";
 }
+
 
 int main()
 {
     try
     {
-        Socket listening_socket{};
+        const Socket listening_socket{};
         listening_socket.set_reuse_addr();
         listening_socket.bind_to(1234);
         listening_socket.listen_for();
