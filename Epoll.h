f18ef88a0bbcecbcf11754dcce938056b6f407fc
Nagul75 <nagul697@gmail.com>
2025-09-25 11:48:20 +0530

    RK-8 encapsulate class in network namespace

--- Epoll.h
+++ Epoll.h
@@ -10,23 +10,27 @@
 #include <vector>
 #include "Socket.h"
 
-class Epoll
+namespace NW
 {
-public:
-    Epoll();
-    ~Epoll();
+    class Epoll
+    {
+    public:
+        Epoll();
+        ~Epoll();
 
-    // Unique resource, so not copyable
-    Epoll(const Epoll&) = delete;
-    Epoll& operator=(const Epoll&) = delete;
+        // Unique resource, so not copyable
+        Epoll(const Epoll&) = delete;
+        Epoll& operator=(const Epoll&) = delete;
 
-    void add(NW::Socket& socket, void* user_data, uint32_t event);
-    void modify(NW::Socket& socket, void* user_data, uint32_t event);
-    void remove(NW::Socket& socket);
-    int wait(std::vector<struct epoll_event>& events, int timeout_ms = -1);
+        void add(const NW::Socket& socket, void* user_data, const uint32_t events);
+        void modify(NW::Socket& socket, void* user_data, uint32_t events);
+        void remove(NW::Socket& socket);
+        int wait(std::vector<struct epoll_event>& events, int timeout_ms = -1);
 
-private:
-    int m_epoll_fd{};
-};
+    private:
+        int m_epoll_fd{};
+    };
+}
+
 
 #endif //REDISKNOCKOFF_EPOLL_H
