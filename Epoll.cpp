7160486d03765b90885d32ad0642f5faa1b0ca4c
Nagul75 <nagul697@gmail.com>
2025-09-25 11:48:49 +0530

    RK-8 implement constructor and destructor

--- Epoll.cpp
+++ Epoll.cpp
@@ -1,3 +1,23 @@
 //
 // Created by regal on 9/24/25.
 //
+
+#include "Epoll.h"
+#include <cstring>
+#include <cerrno>
+
+NW::Epoll::Epoll()
+{
+    m_epoll_fd = epoll_create1(0);
+    if (m_epoll_fd < 0)
+    {
+        throw std::runtime_error("Epoll() - epoll_create1() failed: " + std::string(strerror(errno)));
+    }
+}
+
+NW::Epoll::~Epoll() {
+    // The file descriptor is automatically closed when the object is destroyed.
+    if (m_epoll_fd >= 0) {
+        close(m_epoll_fd);
+    }
+}
